b9aca6d875973ea0a2b3e4f63aba9df6e08590c2
DaniloIS <danilosantos-pg@hotmail.com>
2025-03-02 00:11:58 -0300

    feat: fetch summary

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,33 @@ import { Dialog } from "./components/ui/dialog";
 import { CreateGoal } from "./components/create-goal";
 import { EmptyGoal } from "./components/empty-goal";
 import { Summary } from "./components/summary";
+import { useEffect, useState } from "react";
+
+interface ISummary {
+  completed: number;
+  total: number;
+  goalsPerDay: Record<
+    string,
+    {
+      id: number;
+      title: string;
+      completedAt: string;
+    }[]
+  >;
+}
 
 export function App() {
+  const [summary, setSummary] = useState<ISummary | null>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/summary")
+      .then((response) => response.json())
+      .then(setSummary);
+  }, []);
+
   return (
     <Dialog>
-      {/* <EmptyGoal /> */}
-      <Summary />
+      {summary && summary.total > 0 ? <Summary /> : <EmptyGoal />}
 
       <CreateGoal />
     </Dialog>
